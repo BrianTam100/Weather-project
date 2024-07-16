2e1920966a42d31212d48d9bb9b32eb301d5d24d
BrianTam100 <149264329+BrianTam100@users.noreply.github.com>
2024-07-16 13:34:14 -0400

    Update and rename Create.py to weather.py

--- weather.py
+++ weather.py
@@ -1,28 +1,21 @@
+
 import requests, json
 from tkinter import *
 from functools import partial
 
 in_USA = False 
 
-# Gather U.S. location information
+
 while in_USA == False:
-  user_input = input("Enter an address, street name, zip code, significant landmark, or state in the United States: ") 
+  user_input = input("Enter a location in the United States: ") 
 
-
-
-  # This code takes inspiration from the Nominatim API, which allows users to access the longitude and latitude of a place from its name or address. 
-  # Nominatim 4.2.1. Nominatim API. Accessed March 16, 2023, from https://nominatim.org/release-docs/latest/api/Search/
-  # requests 2.28.2. 2023 Python Software Foundation. Accessed March 16, 2023, from https://requests.readthedocs.io/en/latest/user/quickstart/
-
-# This function returns the exact longitude and latitude of the place that the user entered
+# Call the Nominatim API to retrieve longitude and latitude data for entered location
   def geocode():
-    r = requests.get(f'https://nominatim.openstreetmap.org/search?q={user_input}&format=json')
+    r = requests.get(f'https://nominatim.openstreetmap.org/search?q={user_input}&format=json', headers={'User-Agent': 'Mozilla/5.0'})
     return r 
-
 
   data = geocode().json() 
 
-  # Iterate through elements in the JSON file to find latitude and longitude
   for key in data[0]: 
       if key == "lat":
           lat = (data[0][key]) 
@@ -34,11 +27,10 @@ while in_USA == False:
     in_USA = True
   else: 
     print("Please enter a valid United States address, street name, zip code, significant landmark, or state.") 
-    continue # return to the beginning of the while loop
+    continue 
 
 
-# Use the National Weather Service API to get weather information for the entered location
-# National Weather Service. Weather.gov. Accessed March 16, 2023, from https://www.weather.gov/documentation/services-web-api
+# Retrieve weather data through the National Weather Service's API
 
 url = f'https://api.weather.gov/points/{lat},{long}' 
 
@@ -52,7 +44,6 @@ for key in my_dict:
   if key == "forecast": 
     weather_info = (my_dict[key]) 
 
-# Gather weather data for the location
 r1 = requests.get(weather_info) 
 forecast = json.loads(r1.text) 
 parse = forecast['properties']
@@ -81,27 +72,20 @@ def parse_data():
 
 parse_data()
 
-# Python Software Foundation 2023. tkinter - Python interface. Accessed March 17, 2023 https://docs.python.org/3/library/tkinter.html
-# Takes inspiration from GeeksForGeeks tkinter Python introduction
-# GeeksForGeeks. akshay_sharma08. Accessed March 19, 2023 https://www.geeksforgeeks.org/create-first-gui-application-using-python-tkinter/
-# GeeksForGeeks. abhigoya. Accessed March 19, 2023 https://www.geeksforgeeks.org/how-to-use-images-as-backgrounds-in-tkinter/
-# Stack Overflow. ignoramus. Accessed March 19, 2023. https://stackoverflow.com/questions/2297336/tkinter-specifying-arguments-for-a-function-thats-called-when-you-press-a-butt
-
-# Create a graphical user interface(GUI) using tkinter
+# Create a graphical user interface(GUI) using Tkinter
 root = Tk()
 root.title("Weather Forecaster")
 root.geometry('1920x1080') 
 
-# Set the background of the GUI
+
 background = PhotoImage(file = "weather.png")
 image = Label(root, image = background) 
 image.place(x=0,y=0)
 
-# Create a label to display the user's entered place
 prompt = Label(root, text = f"{entered_place}", bg="sky blue")
 prompt.grid() 
 
-# This function creates buttons for all 14 days
+# Create buttons for each day
 
 def create_button(y):
     btn = Button(root, text = f'{Forecast_Information[y][0]}', command = partial(text, Forecast_Information[y]), width = 13) 
@@ -111,7 +95,7 @@ def create_button(y):
     else: 
         btn.place(x=15, y= (y+1) * 50) 
 
-# This function formats and displays weather information when the button is clicked
+# Display weather data whenever button is clicked
 
 def text(word): 
   text1 = "" 
@@ -133,7 +117,6 @@ def text(word):
     label.place(x = label_dict[a][0], y = label_dict[a][1]) 
 
 
-# Create the buttons for each day
 
 create_button(0) 
 create_button(1) 
@@ -150,6 +133,6 @@ create_button(11)
 create_button(12)
 create_button(13) 
 
-# Tutorialspoint. How to put a Tkinkter window on top of others? Dev Prakash Sharma. Accessed March 19, 2023. https://www.tutorialspoint.com/how-to-put-a-tkinter-window-on-top-of-the-others
-root.attributes('-topmost', True) # make the tkinter window the the active window
-root.mainloop() # start the tkinter window
+
+root.attributes('-topmost', True) 
+root.mainloop() 
